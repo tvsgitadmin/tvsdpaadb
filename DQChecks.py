ca72382376229b0aef4c5da77706fd6d4b0fef6c
tvsgitadmin <tvsgitadmin>
2024-12-20 06:35:47 +0000

    Added notebooks

--- DQChecks.py
+++ DQChecks.py
@@ -1,40 +1,4 @@
 # Databricks notebook source
-config_path = "/Workspace/Repos/sairam1985.sai@hotmail.com/tvsdpaadb/config/config.json"
-
-
-# COMMAND ----------
-
-# Correct path
-config_path = "/Workspace/Repos/sairam1985.sai@hotmail.com/tvsdpaadb/config/config.json"
-
-# Read the file using Databricks file system utilities
-with open(config_path.replace("/dbfs", "/dbfs"), "r") as file:
-    config = json.load(file)
-
-# COMMAND ----------
-
-# DBTITLE 1,Mount ADLS
-config_path = "/Workspace/Repos/sairam1985.sai@hotmail.com/tvsdpaadb/config/config.json"
-
-with open(config_path.replace("/dbfs", "/dbfs"), "r") as file:
-    config = json.load(file)
-
-storage_account_name = config.get("storage_account_name")
-container_name = config.get("container_name")
-source_files_path = config.get("source_files_path")
-
-dbutils.fs.mount(
-    source=f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net/",
-    mount_point="/mnt/tvsdpa",
-    extra_configs={
-        f"fs.azure.account.key.{storage_account_name}.blob.core.windows.net": dbutils.secrets.get(
-            scope="snsttvsdpadevadb", key="scsttvsdpadev"
-        )
-    },
-)
-
-# COMMAND ----------
-
 # List all files in the directory
 files = dbutils.fs.ls(f"/mnt/tvsdpa/{source_files_path}")
 
@@ -53,9 +17,6 @@ if not reference_columns:
     raise ValueError("The 'source_columns' is missing from the configuration.")
 reference_columns_list = [col.strip() for col in reference_columns.split(",")]
 
-print(f"Reference Column Count: {reference_column_count}")
-print(f"Reference Columns: {reference_columns_list}")
-
 # Process each file in the directory
 for file_info in files:
     file_path = file_info.path
@@ -67,8 +28,7 @@ for file_info in files:
         # Get the column count and column names of the current file
         column_count = len(df.columns)
         file_columns = [col.strip() for col in df.columns]
-        print(f"File: {file_path}, Column Count: {column_count}, Columns: {file_columns}")
-
+       
         # Determine if the file matches the reference column count and names
         if column_count == reference_column_count and set(file_columns) == set(reference_columns_list):
             target_folder = config.get("bronze_file_path")
@@ -83,13 +43,6 @@ for file_info in files:
 
         # Move the file to the appropriate folder
         dbutils.fs.mv(file_path, target_path)
-        print(f"Moved File: {file_path} to {target_path}")
-
+        
     except Exception as e:
         print(f"Error processing file: {file_path}. Error: {e}")
-
-
-# COMMAND ----------
-
-# DBTITLE 1,Unmount the mount point
-dbutils.fs.unmount("/mnt/tvsdpa")
